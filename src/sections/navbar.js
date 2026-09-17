80848baaa276d0654261ca48f4e8f0f046c0511d
Hadiya Mariyam <hadiyamariyam00@gmail.com>
2026-09-17 17:05:36 +0530

    add navbar component

---
+++ src/sections/navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <header className="navbar">
+        <a href="#home" className="logo">Hadiya.</a>
+      <nav className="nav-links">
+        <a href="#about">About </a>
+        <a href="#skills">Skills</a>
+        <a href="#projects"> Projects</a>
+        <a href="#contact">Contact</a>
+        <a href="/resume.pdf" download>Resume⬇</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
+
+        
